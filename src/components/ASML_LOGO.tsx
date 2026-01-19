26d553f9a4af753fdb0b7ae5861a89dd6a47d49f
shabtay shalem <shabtay.shalem@gmail.com>
2026-01-19 18:49:23 +0200

    19-01-2026 18:49

--- src/components/ASML_LOGO.tsx
+++ src/components/ASML_LOGO.tsx
@@ -26,7 +26,7 @@ const ASML_LOGO = () => {
         {/* <article className="logo-main"> 
           <div className="logo-parent z-50 w-fit cursor-pointer" onClick={() => navigate("/")}>
           */}
-        <article className="mx-4 my-0 flex px-0 py-2 caret-transparent">
+        <article className="mx-3 my-0 flex px-0 py-2 caret-transparent">
           <div className="w-14">
             {showLOGO && (
               <div className="z-50 mr-0 w-fit cursor-pointer p-1 text-white" onClick={() => navigate("/")}>
@@ -36,7 +36,7 @@ const ASML_LOGO = () => {
             )}
           </div>
 
-          <div className="mr-2 p-1 text-white">
+          <div className="mr-1 p-1 text-white">
             <div className="logo-cci">
               <div>
                 <FaLock style={{ padding: "0" }} />
@@ -46,7 +46,7 @@ const ASML_LOGO = () => {
             <div className="select-none pt-[2px] text-[0.75rem] font-semibold">2.4.0</div>
             {/* <div className="logo-version">2.4.0</div> */}
           </div>
-          <div className="h-10 w-2 border-l-[1px] border-solid border-[#808080]"></div>
+          <div className="h-10 border-l-[1px] border-solid border-[#808080]"></div>
         </article>
       </section>
     </div>
